d997b5760134f0a824dbe426efdde1c22697c70f
Avinash Adluri <avinashadluri@gmail.com>
2020-04-20 23:22:41 +1000

    build added

---
+++ precache-manifest.a998df7407af6a756d087ab9cfa42a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7442d1a7e9ffa0009bb23947e2d6ada",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "bfbf1b63df5349083003",
+    "url": "/covid-19-tracker/static/css/main.c0528757.chunk.css"
+  },
+  {
+    "revision": "50349d7250dd474ec86c",
+    "url": "/covid-19-tracker/static/js/2.ac724f18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.ac724f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbf1b63df5349083003",
+    "url": "/covid-19-tracker/static/js/main.97c745a0.chunk.js"
+  },
+  {
+    "revision": "69260950a26d9413a7f9",
+    "url": "/covid-19-tracker/static/js/runtime-main.2dc355e1.js"
+  }
+]);
