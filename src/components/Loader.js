7d8ae18a95391e1d47ca9f3cd768089351de93a7
Avinash Adluri <avinashadluri@gmail.com>
2020-04-21 10:53:33 +1000

    moved code

--- src/components/Loader.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-function Loader() {
-  return <div className="spinner"></div>;
-}
-export default Loader;
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Card } from './Card';
-export { default as Footer } from './Footer';
-export { default as Header } from './Header';
-export { default as Stats } from './Stats';
-export { default as CountrySelector } from './CountrySelector';
-export { default as CountryItem } from './CountryItem';
-export { default as CountryList } from './CountryList';
-export { default as Tabs } from './Tabs';
